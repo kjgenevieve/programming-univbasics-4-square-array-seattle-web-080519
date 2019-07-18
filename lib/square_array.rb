39dc6ce71a4e8b9bc4871d6989b1a20dd19501d7
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-18 02:21:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,20 @@
 def square_array(array)
   counter = 0
+  new_array = Array.new
   
   while counter < array.length do
-    new_array.Array
-    array(counter) * array(counter)
+    new_array << array(counter) * array(counter)
     counter += 1
   end
 end
 
 
 
-	def output_array_elements(array)
-			counter = 0
+# 	def output_array_elements(array)
+# 			counter = 0
 		
-			while counter < array.length do
-				puts array[counter]
-				counter += 1
-			end
-end
+# 			while counter < array.length do
+# 				puts array[counter]
+# 				counter += 1
+# 			end
+# end
