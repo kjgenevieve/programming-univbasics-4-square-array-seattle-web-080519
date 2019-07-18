a4f94c854882dad56860cf242d6e36da2903f165
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-18 02:36:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,29 @@
+# def square_array(array)
+#   counter = 0
+#   new_array = []
+  
+#   while counter < array.length do
+#     new_array.push((array[counter] * array[counter]))
+#     puts new_array
+#     counter += 1
+#   end
+# end
+
+
 def square_array(array)
   counter = 0
-  new_array = []
+  # new_array = []
   
   while counter < array.length do
-    new_array.push((array[counter] * array[counter]))
-    puts new_array
+    # new_array.push(array[counter] * array[counter])
+    # p new_array
+
+    p array[counter] * array[counter]
+    
     counter += 1
   end
 end
+
 
 
 # 	def output_array_elements(array)
