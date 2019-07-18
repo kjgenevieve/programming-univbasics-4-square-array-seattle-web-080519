c30bec60fa8872ac9606bdc02ff916f823158036
Genevieve McAllister <kj.genevieve@gmail.com>
2019-07-18 02:24:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-def square_array(numbers)
+def square_array(array)
   counter = 0
   new_array = []
   
   while counter < array.length do
-    new_array << array(counter) * array(counter)
+    # new_array << array(counter) * array(counter)
     counter += 1
   end
 end
